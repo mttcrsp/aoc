801dc5f61cb7a296bfa00b9aba92d810a48cce92
Matteo Crespi <crespi.matteo@icloud.com>
2023-12-03 23:46:58 +0100

    feat(22): day 3 solutions

---
+++ 2022/03/main.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+enum UnexpectedError: Error {
+  case inputNotFound
+  case invalidCharacter
+  case invalidIntersection
+}
+
+extension Character {
+  func priority() throws -> Int {
+    let base: (value: Int, character: Character) =
+      isLowercase ? (1, "a") : (27, "A")
+    guard let asciiValue, let baseAsciiValue = base.character.asciiValue
+    else { throw UnexpectedError.invalidCharacter }
+    return Int(asciiValue)-Int(baseAsciiValue)+base.value
+  }
+}
+
+func part1() async throws -> Int {
+  guard let file = FileHandle(forReadingAtPath: "input/1.txt")
+  else { throw UnexpectedError.inputNotFound }
+
+  var prioritiesSum = 0
+  for try await line in file.bytes.lines {
+    let compartment1 = Set(line.prefix(line.count/2))
+    let compartment2 = Set(line.suffix(line.count/2))
+    let intersection = compartment1.intersection(compartment2)
+    guard let item = intersection.first, intersection.count == 1
+    else { throw UnexpectedError.invalidIntersection }
+    prioritiesSum += try item.priority()
+  }
+
+  return prioritiesSum
+}
+
+func part2() async throws -> Int {
+  guard let file = FileHandle(forReadingAtPath: "input/2.txt")
+  else { throw UnexpectedError.inputNotFound }
+
+  var groups: [Int: Set<Character>] = [:]
+  var index = 0
+  for try await line in file.bytes.lines {
+    defer { index += 1 }
+
+    let items = Set(line)
+    let groupIndex = index/3
+    if let group = groups[groupIndex] {
+      groups[groupIndex] = group.intersection(items)
+    } else {
+      groups[groupIndex] = items
+    }
+  }
+
+  var prioritiesSum = 0
+  for group in groups.values {
+    guard let item = group.first, group.count == 1
+    else { throw UnexpectedError.invalidIntersection }
+    prioritiesSum += try item.priority()
+  }
+
+  return prioritiesSum
+}
