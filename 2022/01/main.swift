d51bb852c5f66059a57a5ac52edf7c98349819da
Matteo Crespi <crespi.matteo@icloud.com>
2023-12-03 21:20:26 +0100

    feat(22): day 1 solutions

---
+++ 2022/01/main.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+enum UnexpectedError: Error {
+  case malformedLine
+}
+
+func part1() throws -> Int {
+  let url = URL(fileURLWithPath: "input/1.txt")
+  let string = try String(contentsOf: url)
+  let lines = string.components(separatedBy: "\n")
+
+  var maxCalories = 0
+  var currentElfCalories = 0
+  for line in lines {
+    if line.isEmpty {
+      maxCalories = Swift.max(currentElfCalories, maxCalories)
+      currentElfCalories = 0
+    } else if let calories = Int(line) {
+      currentElfCalories += calories
+    } else {
+      throw UnexpectedError.malformedLine
+    }
+  }
+
+  return maxCalories
+}
+
+func part2() throws -> Int {
+  let url = URL(fileURLWithPath: "input/2.txt")
+  let string = try String(contentsOf: url)
+  let lines = string.components(separatedBy: "\n")
+
+  var top3CaloriesElves: [Int] = [0, 0, 0]
+  var currentElfCalories = 0
+  for line in lines {
+    if line.isEmpty {
+      top3CaloriesElves.append(currentElfCalories)
+      top3CaloriesElves.sort(by: >)
+      top3CaloriesElves.removeLast()
+      currentElfCalories = 0
+    } else if let calories = Int(line) {
+      currentElfCalories += calories
+    } else {
+      throw UnexpectedError.malformedLine
+    }
+  }
+  return top3CaloriesElves.reduce(0, +)
+}
