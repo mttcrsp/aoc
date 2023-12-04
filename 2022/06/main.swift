3a4d6ec9d30ad669a979b4f879ea0dbe2fad8592
Matteo Crespi <crespi.matteo@icloud.com>
2023-12-04 21:11:13 +0100

    feat(22): day 6 solutions

---
+++ 2022/06/main.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+enum UnexpectedError: Error {
+  case inputNotFound
+  case markerNotFound
+}
+
+func endIndexForMarker(ofLength length: Int, in string: String) throws -> Int {
+  for index in string.indices.dropFirst(length-1) {
+    let lowerBound = string.index(index, offsetBy: -(length-1))
+    let upperBound = index
+    let characterSet = Set(string[lowerBound ... upperBound])
+    if characterSet.count == length {
+      return string.distance(from: string.startIndex, to: index)+1
+    }
+  }
+  throw UnexpectedError.markerNotFound
+}
+
+func part1() throws -> Int {
+  let string = try String(contentsOfFile: "input/1.txt")
+  return try endIndexForMarker(ofLength: 4, in: string)
+}
+
+func part2() throws -> Int {
+  let string = try String(contentsOfFile: "input/2.txt")
+  return try endIndexForMarker(ofLength: 14, in: string)
+}
