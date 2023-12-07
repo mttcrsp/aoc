7099629ae12db423c383f5f45a8c3595b834a35c
Matteo Crespi <crespi.matteo@icloud.com>
2023-12-07 03:47:18 +0100

    feat(22): day 8 solutions

---
+++ 2022/08/main.swift
@@ -0,0 +1,139 @@
+import Foundation
+
+enum UnexpectedError: Error {
+  case inputNotFound
+  case invalidCharacter
+}
+
+enum Direction: CaseIterable {
+  case n, s, w, e
+}
+
+struct Location {
+  let row: Int
+  let col: Int
+  init(_ row: Int, _ col: Int) {
+    self.row = row
+    self.col = col
+  }
+
+  func adjacent(in direction: Direction) -> Location {
+    switch direction {
+    case .n: return .init(row-1, col)
+    case .s: return .init(row+1, col)
+    case .w: return .init(row, col-1)
+    case .e: return .init(row, col+1)
+    }
+  }
+}
+
+struct Forest {
+  var heights: [[Int]]
+  var rows: Int { heights.count }
+  var cols: Int { heights.first?.count ?? 0 }
+  var treesCount: Int { rows*cols }
+
+  init() async throws {
+    guard let file = FileHandle(forReadingAtPath: "input.txt")
+    else { throw UnexpectedError.inputNotFound }
+
+    heights = []
+    for try await line in file.bytes.lines {
+      var row: [Int] = []
+      for character in line {
+        guard let height = Int(String(character))
+        else { throw UnexpectedError.invalidCharacter }
+        row.append(height)
+      }
+      heights.append(row)
+    }
+  }
+
+  func height(at location: Location) -> Int {
+    heights[location.row][location.col]
+  }
+
+  func adjacent(to location: Location, in direction: Direction) -> Location? {
+    let adjacent = location.adjacent(in: direction)
+    guard adjacent.row >= 0, adjacent.row < rows else { return nil }
+    guard adjacent.col >= 0, adjacent.col < cols else { return nil }
+    return adjacent
+  }
+
+  func isTreeVisible(at location: Location, from direction: Direction) -> Bool {
+    var current = location
+    while let adjacent = adjacent(to: current, in: direction) {
+      defer { current = adjacent }
+      if height(at: adjacent) >= height(at: location) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  func isTreeVisible(at location: Location) -> Bool {
+    for direction in Direction.allCases {
+      if isTreeVisible(at: location, from: direction) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  func scenicScore(for location: Location, in direction: Direction) -> Int {
+    var score = 0
+    var current = location
+    while let adjacent = adjacent(to: current, in: direction) {
+      defer { current = adjacent }
+      score += 1
+      if height(at: adjacent) >= height(at: location) {
+        break
+      }
+    }
+
+    return score
+  }
+
+  func scenicScore(for location: Location) -> Int {
+    var score = 1
+    for direction in Direction.allCases {
+      score *= scenicScore(for: location, in: direction)
+    }
+
+    return score
+  }
+}
+
+func part1() async throws -> Int {
+  let forest = try await Forest()
+
+  var nonVisibleTreesCount = 0
+  for row in 1 ..< forest.rows-1 {
+    for col in 1 ..< forest.cols-1 {
+      let location = Location(row, col)
+      let isVisible = forest.isTreeVisible(at: location)
+      if !isVisible {
+        nonVisibleTreesCount += 1
+      }
+    }
+  }
+
+  return forest.treesCount-nonVisibleTreesCount
+}
+
+func part2() async throws -> Int {
+  let forest = try await Forest()
+
+  var maximumScenicScore = 0
+  for row in 1 ..< forest.rows-1 {
+    for col in 1 ..< forest.cols-1 {
+      let location = Location(row, col)
+      let score = forest.scenicScore(for: location)
+      maximumScenicScore = max(maximumScenicScore, score)
+    }
+  }
+
+  return maximumScenicScore
+}
