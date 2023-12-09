b3fe1614c768881d565e64c903587ee5874394e4
Matteo Crespi <crespi.matteo@icloud.com>
2023-12-09 22:11:19 +0100

    refactor(23) simplify day 9

--- 2023/09.swift
+++ 2023/09.swift
@@ -4,59 +4,39 @@ enum UnexpectedError: Error {
   case inputNotFound
 }
 
-enum PredictionType {
-  case prev
-  case next
-}
+guard let file = FileHandle(forReadingAtPath: "09.in")
+else { throw UnexpectedError.inputNotFound }
 
-func prediction(of mode: PredictionType) async throws -> Int {
-  guard let file = FileHandle(forReadingAtPath: "09.in")
-  else { throw UnexpectedError.inputNotFound }
+var prevResult = 0
+var nextResult = 0
 
-  var prevResult = 0
-  var nextResult = 0
-
-  for try await line in file.bytes.lines {
-    let scanner = Scanner(string: line)
-    var numbers: [Int] = []
-    while let number = scanner.scanInt() {
-      numbers.append(number)
-    }
-
-    var history: [[Int]] = [numbers]
-    while let prev = history.last, !prev.allSatisfy({ $0 == 0 }) {
-      var next: [Int] = []
-      for i in prev.indices.dropFirst() {
-        next.append(prev[i]-prev[i-1])
-      }
-      history.append(next)
-    }
-
-    var prevPrediction = 0
-    var nextPrediction = 0
-    for entry in history.reversed().dropFirst() {
-      let prev = entry[0]
-      let next = entry[entry.count-1]
-      prevPrediction = prev-prevPrediction
-      nextPrediction = next+nextPrediction
-    }
-    prevResult += prevPrediction
-    nextResult += nextPrediction
+for try await line in file.bytes.lines {
+  let scanner = Scanner(string: line)
+  var numbers: [Int] = []
+  while let number = scanner.scanInt() {
+    numbers.append(number)
   }
 
-  switch mode {
-  case .prev: return prevResult
-  case .next: return nextResult
+  var history: [[Int]] = [numbers]
+  while let prev = history.last, !prev.allSatisfy({ $0 == 0 }) {
+    var next: [Int] = []
+    for i in prev.indices.dropFirst() {
+      next.append(prev[i]-prev[i-1])
+    }
+    history.append(next)
   }
+
+  var prevPrediction = 0
+  var nextPrediction = 0
+  for entry in history.reversed().dropFirst() {
+    let prev = entry[0]
+    let next = entry[entry.count-1]
+    prevPrediction = prev-prevPrediction
+    nextPrediction = next+nextPrediction
+  }
+  prevResult += prevPrediction
+  nextResult += nextPrediction
 }
 
-func part1() async throws -> Int {
-  try await prediction(of: .next)
-}
-
-func part2() async throws -> Int {
-  try await prediction(of: .prev)
-}
-
-try await print(part1())
-try await print(part2())
+print(nextResult)
+print(prevResult)
