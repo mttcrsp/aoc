13cce8a5c5fa15bf65929113640360e719b25d46
Matteo Crespi <crespi.matteo@icloud.com>
2024-11-16 21:19:25 +0100

    refactor: playground input file loading

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,8 +1,6 @@
 import Foundation
 
-guard
-  let fileURL = Bundle.module.url(forResource: "ex", withExtension: nil),
-  let file = try? FileHandle(forReadingFrom: fileURL)
+guard let file = FileHandle(forReadingAtPath: "Sources/Resources/ex")
 else { fatalError("input not found") }
 
 for try await line in file.bytes.lines {
