e4f6bafc1782675d3d84adcead2efde6fe594c06
Matteo Crespi <crespi.matteo@icloud.com>
2024-12-05 22:23:14 +0100

    refactor: change input via args

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,10 @@
 import Foundation
 
-guard let input = try? String(contentsOfFile: "Sources/Resources/ex", encoding: .utf8)
+let args = ProcessInfo.processInfo.arguments
+let file = args.contains("ex") ? "ex" : "in"
+let path = "Sources/Resources/\(file)"
+
+guard let input = try? String(contentsOfFile: path, encoding: .utf8)
 else { fatalError("input not found") }
 
 let lines = input.components(separatedBy: "\n")
