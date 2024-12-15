daac27e4d8a1cc8b407f32bf25c558a0d6795d6b
Matteo Crespi <crespi.matteo@icloud.com>
2024-12-15 23:03:59 +0100

    chore: swift-algorithms 1.2.0

--- Package.swift
+++ Package.swift
@@ -7,12 +7,14 @@ let package = Package(
   platforms: [.macOS(.v14)],
   dependencies: [
     .package(url: "https://github.com/apple/swift-collections.git", from: .init(1, 1, 4)),
+    .package(url: "https://github.com/apple/swift-algorithms.git", from: .init(1, 2, 0)),
   ],
   targets: [
     .executableTarget(
       name: "aoc",
       dependencies: [
         .product(name: "Collections", package: "swift-collections"),
+        .product(name: "Algorithms", package: "swift-algorithms"),
       ],
       cSettings: [
         .define("ACCELERATE_NEW_LAPACK"),
