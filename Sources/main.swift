32b9e14c17a0fe72c41ab1419bc4374a05acc95d
Matteo Crespi <crespi.matteo@icloud.com>
2024-12-20 00:51:43 +0100

    refactor: grid utilities by default

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,6 +2,48 @@ import Algorithms
 import Collections
 import Foundation
 
+enum Direction: CaseIterable {
+  case up, down, left, right
+  var delta: (row: Int, col: Int) {
+    switch self {
+    case .up: (-1, 0)
+    case .down: (+1, 0)
+    case .left: (0, -1)
+    case .right: (0, +1)
+    }
+  }
+}
+
+struct Point: Hashable {
+  var row, col: Int
+  init(_ row: Int, _ col: Int) {
+    self.row = row
+    self.col = col
+  }
+
+  func next(in direction: Direction) -> Point {
+    var next = self
+    next.row += direction.delta.row
+    next.col += direction.delta.col
+    return next
+  }
+}
+
+extension [[Character]] {
+  var rowsCount: Int { count }
+  var colsCount: Int { self[0].count }
+  var rowsIndices: Range<Int> { indices }
+  var colsIndices: Range<Int> { self[0].indices }
+  func contains(_ point: Point) -> Bool {
+    rowsIndices ~= point.row && colsIndices ~= point.col
+  }
+
+  subscript(_ point: Point) -> Character {
+    get { self[point.row][point.col] }
+    set { self[point.row][point.col] = newValue }
+  }
+}
+
 let args = ProcessInfo.processInfo.arguments
 let file = args.contains("ex") ? "ex" : "in"
 
